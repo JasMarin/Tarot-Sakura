4cec7e6b13c60d417162e1fa4dea1c37b87c32c5
JasMarin <yasminamarin17@gmail.com>
2022-12-16 22:35:01 +0100

    componente home y header agregados

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header(props) {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header;
+Header.defaultProps = {
+    class:"header"
+   
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+function Home() {
+  return (
+    <div>
+        <Header class= >
+    </div>
+  )
+}
+
+export default Home
