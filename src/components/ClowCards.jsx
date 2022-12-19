756cf11e6efb6779a8e7c85328480e6b4e79807f
Elvia Benedith <ejbenedith@gmail.com>
2022-12-19 11:43:03 +0100

    Componet cardcontext

--- src/components/ClowCards.jsx
+++ src/components/ClowCards.jsx
@@ -1,28 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import { CardsContext } from '../context/CardsContext';
 import {GetCards} from '../services/Functions'
 import '../styles/ClowCards.css'
 
 
-function ClowCards() {
+function ClowCards ()  {
+   
     const [cards, setCards] = useState(null);
-
+    console.log(addToLecture);
     useEffect(()=>{
         GetCards(setCards)
       },[]);
-    
-
+  
+    const {addToLecture} = useContext(CardsContext);
   return (
-    <div className='cards-continer'>
+     <div className='cards-continer background'>
         {cards !== null ? (
         cards.map(card => (
           
             <div key={card.id}>
-                <img className='cards-img' src={card.cardsReverse.clowReverse} alt={card.spanishName}/>
+                <img className='cards-img' src={card.cardsReverse.clowReverse} alt={card.spanishName} onClick={() => addToLecture(card)}/>
             </div>
          
         ))
             ) : ('No hay cards')}
-    </div>
+     </div>
   )
 }
 
